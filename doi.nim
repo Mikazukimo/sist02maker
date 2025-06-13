bbaff16296eb7a655423a924462a7a711941d2ed
Mikazukimo <mikazukimo2525@yahoo.co.jp>
2025-06-13 20:13:28 +0900

    doiからの検索に対応したはずです

--- doi.nim
+++ doi.nim
@@ -5,14 +5,43 @@ proc doi_searcher(doi:string):string=
     let queryParams = doi
     echo DOI_JaLC_URL & queryParams
     try:
+        var title, author, publisher, date,volume,issue,first_page,last_page: string
+        var author_list : seq[string]
         let client = newHttpClient()
-        let response = %* client.getContent(DOI_JaLC_URL & queryParams)
-        echo $response["status"]
+        let response = client.getContent(DOI_JaLC_URL & queryParams)
+        let jsonNode = parseJson(response)
+        doAssert jsonNode.kind == JObject
+        #title取得
+        title = jsonNode["data"]["title_list"][0]["title"].getStr
+        #author取得
+        var i = 0
+        while true:
+            try :
+                var last_name,first_name:string
+                var x = 0
+                for i in jsonNode["data"]["creator_list"][i]["names"]:
+                    if x == 1:
+                        break
+                    else :
+                        last_name = i["last_name"].getStr
+                        first_name = i["first_name"].getStr
+                        author_list.add(last_name&" "&first_name)
+                        x = x+1
+                i = i+1
+            except IndexDefect:
+                break
+        author = author_list.join(", ")
+        #publisher, date,volume,issue,first_page,last_page取得
+        publisher = jsonNode["data"]["journal_title_name_list"][0]["journal_title_name"].getStr
+        date = jsonNode["data"]["recorded_year"].getStr
+        volume = jsonNode["data"]["volume"].getStr
+        issue = jsonNode["data"]["issue"].getStr
+        first_page = jsonNode["data"]["first_page"].getStr
+        last_page = jsonNode["data"]["last_page"].getStr
+        echo author & ". " & title & ". " & publisher & ", " & date & ", vol." & volume & ", no." & issue & ", p." & first_page & "-" & last_page & "."
     except HttpRequestError as e:
         echo "リクエスト失敗: ", e.msg
         let ans = "ERROR"
         return ans
-
-
-let query = "10.11514/infopro.2008.0.138.0"
+let query = "10.20651/jslis.65.2_84"
 discard doi_searcher(query)
--- nim_lang/src/sist02maker.nim
+++ nim_lang/src/sist02maker.nim
@@ -1,5 +1,6 @@
-import jester,nimja,os,httpclient,strutils,xmlparser,xmltree
+import jester,nimja,os,httpclient,strutils,xmlparser,xmltree,json
 const NDL_URL = "https://ndlsearch.ndl.go.jp/api/opensearch?"
+const DOI_JaLC_URL = "https://api.japanlinkcenter.org/dois/"
 
 proc isbn_searcher(isbn:string):string=
   let queryParams = "isbn='"&isbn&"'"
@@ -33,11 +34,54 @@ proc isbn_searcher(isbn:string):string=
       let ans = "ERROR"
       return ans
 
+proc doi_searcher(doi:string):string=
+    let queryParams = doi
+    echo DOI_JaLC_URL & queryParams
+    try:
+        var title, author, publisher, date,volume,issue,first_page,last_page: string
+        var author_list : seq[string]
+        let client = newHttpClient()
+        let response = client.getContent(DOI_JaLC_URL & queryParams)
+        let jsonNode = parseJson(response)
+        doAssert jsonNode.kind == JObject
+        #title取得
+        title = jsonNode["data"]["title_list"][0]["title"].getStr
+        #author取得
+        var i = 0
+        while true:
+            try :
+                var last_name,first_name:string
+                var x = 0
+                for i in jsonNode["data"]["creator_list"][i]["names"]:
+                    if x == 1:
+                        break
+                    else :
+                        last_name = i["last_name"].getStr
+                        first_name = i["first_name"].getStr
+                        author_list.add(last_name&" "&first_name)
+                        x = x+1
+                i = i+1
+            except IndexDefect:
+                break
+        author = author_list.join(", ")
+        #publisher, date,volume,issue,first_page,last_page取得
+        publisher = jsonNode["data"]["journal_title_name_list"][0]["journal_title_name"].getStr
+        date = jsonNode["data"]["recorded_year"].getStr
+        volume = jsonNode["data"]["volume"].getStr
+        issue = jsonNode["data"]["issue"].getStr
+        first_page = jsonNode["data"]["first_page"].getStr
+        last_page = jsonNode["data"]["last_page"].getStr
+        echo author & ". " & title & ". " & publisher & ", " & date & ", vol." & volume & ", no." & issue & ", p." & first_page & "-" & last_page & "."
+    except HttpRequestError as e:
+        echo "リクエスト失敗: ", e.msg
+        let ans = "ERROR"
+        return ans
+
 
 proc renderIndex(title:string): string =
   compileTemplateFile("./templates/index.nimja", baseDir = getScriptDir)
 
-proc renderAns(ans:string): string =
+proc renderAns(respons_isbn:string,respons_doi:string): string =
   compileTemplateFile("./templates/ans.nimja", baseDir = getScriptDir)
 
 routes:
@@ -46,9 +90,11 @@ routes:
     resp renderIndex(title)
 
   post "/sist02":
-    let raw: string = request.params["isbn"]
-    let isbn = raw.replace("-", "")
-    let ans = isbn_searcher(isbn)
-    resp renderAns(ans)
+    let raw_isbn: string = request.params["isbn"]
+    let raw_doi: string = request.params["doi"]
+    let isbn = raw_isbn.replace("-", "")
+    let respons_isbn = isbn_searcher(isbn)
+    let respons_doi = doi_searcher(raw_doi)
+    resp renderAns(respons_isbn,respons_doi)
 
 runForever()
